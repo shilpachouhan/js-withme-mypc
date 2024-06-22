c51548c69d5636064e10d7c567541e3ff352d28d
shilpachouhan <chouhanshilpa545@gmail.com>
2024-06-22 14:42:22 +0000

    datatypes

---
+++ 00_basic/variable.js
@@ -0,0 +1,7 @@
+const accountId =144553
+let accountEmail ="chouhanshilpa545@gmail.com"
+var accountPassword ="123456"
+accountCity ="Delhi"
+accountHoldername = "shilpa"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountHoldername])
